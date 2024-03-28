4ca319dd61513b08e48cd880d080a4e0d6976813
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-03-28 15:11:15 -0400

    Add simple game canvas component

---
+++ src/components/GameCanvas/GameCanvas.tsx
@@ -0,0 +1,5 @@
+const GameCanvas = ({ height, width }: { height: number; width: number }) => {
+  return <canvas className="bg-neutral-500 " height={height} width={width} />;
+};
+
+export default GameCanvas;
