66bfa099abed0f08b67d6c6508c3cfd4be9a99bc
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-03-28 17:19:35 -0400

    Add interface for returned object

--- src/components/GameCanvas/useCellData.ts
+++ src/components/GameCanvas/useCellData.ts
@@ -1,5 +1,12 @@
 import { useRef } from "react";
 
+export interface CellData {
+  current: Int8Array;
+  next: Int8Array;
+  initialize: () => void;
+  computeNext: () => void;
+}
+
 const useCellData = (gridSize: number) => {
   const currentValues = useRef(new Int8Array(gridSize * gridSize));
   const nextValues = useRef(new Int8Array(gridSize * gridSize));
@@ -17,7 +24,7 @@ const useCellData = (gridSize: number) => {
     // Use GoL rules to determine next state
   };
 
-  const cellData = {
+  const cellData: CellData = {
     current: currentValues.current,
     next: nextValues.current,
     initialize,
