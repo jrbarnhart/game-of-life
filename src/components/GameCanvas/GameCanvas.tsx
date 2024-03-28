93104d9313aa4eae0887e49e262947df593886ae
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-03-28 18:18:55 -0400

    Implement custom hooks

--- src/components/GameCanvas/GameCanvas.tsx
+++ src/components/GameCanvas/GameCanvas.tsx
@@ -1,5 +1,24 @@
+import useCellAnimation from "./useCellAnimation";
+import useCellData from "./useCellData";
+import useInitCanvas from "./useInitCanvas";
+
 const GameCanvas = ({ height, width }: { height: number; width: number }) => {
-  return <canvas className="bg-neutral-500 " height={height} width={width} />;
+  const cellData = useCellData(10);
+  const { canvasInitialized, canvasRef, contextRef } = useInitCanvas();
+  useCellAnimation(
+    canvasRef.current,
+    contextRef.current,
+    canvasInitialized,
+    cellData
+  );
+  return (
+    <canvas
+      className="bg-neutral-500 "
+      height={height}
+      width={width}
+      ref={canvasRef}
+    />
+  );
 };
 
 export default GameCanvas;
