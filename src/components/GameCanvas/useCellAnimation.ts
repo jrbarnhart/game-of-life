7f4c60977a8f69d1b3f1040eb16b763f209bb111
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-03-28 20:42:00 -0400

    Put cell draw into helper fn for DRY

--- src/components/GameCanvas/useCellAnimation.ts
+++ src/components/GameCanvas/useCellAnimation.ts
@@ -13,25 +13,30 @@ const useCellAnimation = (
     (options?: { initialDraw: boolean }) => {
       // All grids are square so this is sqrt of total length
       const rowsAndCols = Math.sqrt(cellData.current.length);
-      const cellSize = canvas ? canvas.width / rowsAndCols : null;
+      const cellSize = canvas ? canvas.width / rowsAndCols : 0;
 
       // Compute the next cell data state
       cellData.computeNext();
+
+      // Helper fn for drawing cells
+      const drawCell = (ctx: CanvasRenderingContext2D, i: number) => {
+        const row = Math.floor(i / rowsAndCols);
+        const col = i % rowsAndCols;
+        const x = col * cellSize;
+        const y = row * cellSize;
+
+        ctx.fillStyle = cellData.next[i] === 0 ? "black" : "white";
+        ctx.beginPath();
+        ctx.rect(x, y, cellSize, cellSize);
+        ctx.fill();
+      };
 
       // If restarting animation clear it and redraw current cell state
       if (options?.initialDraw && ctx && canvas) {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         for (let i = 0; i < cellData.current.length; i++) {
           if (cellSize) {
-            const row = Math.floor(i / rowsAndCols);
-            const col = i % rowsAndCols;
-            const x = col * cellSize;
-            const y = row * cellSize;
-
-            ctx.fillStyle = cellData.next[i] === 0 ? "black" : "white";
-            ctx.beginPath();
-            ctx.rect(x, y, cellSize, cellSize);
-            ctx.fill();
+            drawCell(ctx, i);
           }
         }
       } else {
@@ -39,15 +44,7 @@ const useCellAnimation = (
           if (cellData.current[i] === cellData.next[i]) {
             continue;
           } else if (cellSize && ctx) {
-            const row = Math.floor(i / rowsAndCols);
-            const col = i % rowsAndCols;
-            const x = col * cellSize;
-            const y = row * cellSize;
-
-            ctx.fillStyle = cellData.next[i] === 0 ? "black" : "white";
-            ctx.beginPath();
-            ctx.rect(x, y, cellSize, cellSize);
-            ctx.fill();
+            drawCell(ctx, i);
           }
         }
       }
