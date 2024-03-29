9b2a736f4faee57c2688f384fea536be06c8adca
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-03-29 14:02:56 -0400

    Fix test file dir

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,6 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "jsdom",
-    setupFiles: "./__tests__/setup.js",
+    setupFiles: "./src/__tests__/setup.ts",
   },
 });
