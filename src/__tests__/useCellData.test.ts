c7b464c6ee434cea9eb3e909bb02707a006c6407
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-03-29 18:08:51 -0400

    Test that initial data is set correctly

--- src/__tests__/useCellData.test.ts
+++ src/__tests__/useCellData.test.ts
@@ -4,9 +4,26 @@ import useCellData from "../components/GameCanvas/useCellData";
 
 describe("useCellData", () => {
   it("returns object with correct properties", () => {
-    const { result } = renderHook(() => useCellData(2));
+    const { result } = renderHook(() => useCellData(0));
     expect(result.current.current).toBeInstanceOf(Int8Array);
     expect(result.current.next).toBeInstanceOf(Int8Array);
     expect(result.current.computeNext).toBeTypeOf("function");
+  });
+
+  it("correctly initializes passed data", () => {
+    const gridSize = 3;
+    const initialData = [0, 1, 0, 1, 1, 0, 0, 0, 1];
+    const { result } = renderHook(() => useCellData(gridSize, initialData));
+    expect(result.current.current[1]).toBe(1);
+  });
+
+  describe("computeNext", () => {
+    it("correctly swap betweens arrays for curent and next", () => {
+      const gridSize = 3;
+      const initialData = [0, 1, 0, 1, 1, 0, 0, 0, 1];
+      const { result, rerender } = renderHook(() =>
+        useCellData(gridSize, initialData)
+      );
+    });
   });
 });
