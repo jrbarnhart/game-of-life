cd4600d16b8c9758456561fca0a70387980699d7
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-03-29 20:16:58 -0400

    Remove unused test

--- src/__tests__/useCellData.test.ts
+++ src/__tests__/useCellData.test.ts
@@ -14,16 +14,7 @@ describe("useCellData", () => {
     const gridSize = 3;
     const initialData = [0, 1, 0, 1, 1, 0, 0, 0, 1];
     const { result } = renderHook(() => useCellData(gridSize, initialData));
-    expect(result.current.current[1]).toBe(1);
-  });
-
-  describe("computeNext", () => {
-    it("correctly swap betweens arrays for curent and next", () => {
-      const gridSize = 3;
-      const initialData = [0, 1, 0, 1, 1, 0, 0, 0, 1];
-      const { result, rerender } = renderHook(() =>
-        useCellData(gridSize, initialData)
-      );
-    });
+    const currentArray = Array.from(result.current.current);
+    expect(currentArray).toStrictEqual([0, 1, 0, 1, 1, 0, 0, 0, 1]);
   });
 });
