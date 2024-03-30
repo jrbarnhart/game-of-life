1cf63d30841cb92b2fd0011ac683d78418d5fd38
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-03-30 16:34:43 -0400

    Rewrite initialization test for new approach

--- src/__tests__/useCellData.test.ts
+++ src/__tests__/useCellData.test.ts
@@ -5,16 +5,24 @@ import useCellData from "../components/GameCanvas/useCellData";
 describe("useCellData", () => {
   it("returns object with correct properties", () => {
     const { result } = renderHook(() => useCellData(0));
-    expect(result.current.current).toBeInstanceOf(Int8Array);
-    expect(result.current.next).toBeInstanceOf(Int8Array);
+    expect(result.current.gameState).toBeInstanceOf(Uint8Array);
+    expect(result.current.changedCells).toBeInstanceOf(Uint32Array);
     expect(result.current.computeNext).toBeTypeOf("function");
   });
 
-  it("correctly initializes passed data", () => {
-    const gridSize = 3;
-    const initialData = [0, 1, 0, 1, 1, 0, 0, 0, 1];
-    const { result } = renderHook(() => useCellData(gridSize, initialData));
-    const currentArray = Array.from(result.current.current);
-    expect(currentArray).toStrictEqual([0, 1, 0, 1, 1, 0, 0, 0, 1]);
+  /*
+    Grid representation:
+    0 0 1 0
+    0 1 0 0
+    0 0 1 0
+    1 0 0 0
+  */
+  it("returns proper gameState based on initialData", () => {
+    const { result } = renderHook(() =>
+      useCellData(4, [0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0])
+    );
+    expect(result.current.gameState).toBe([
+      [2, 3, 129, 2, 1, 130, 3, 2, 2, 3, 129, 2, 128, 3, 2, 3],
+    ]);
   });
 });
