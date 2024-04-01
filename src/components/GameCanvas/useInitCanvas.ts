bdfea533023aec8c23c7df3bd8db6137affa5c8d
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-04-01 06:22:44 -0400

    Init overlay canvas

--- src/components/GameCanvas/useInitCanvas.ts
+++ src/components/GameCanvas/useInitCanvas.ts
@@ -3,19 +3,28 @@ import { useEffect, useRef, useState } from "react";
 const useInitCanvas = () => {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
   const contextRef = useRef<CanvasRenderingContext2D | null>(null);
+  const overlayRef = useRef<HTMLCanvasElement | null>(null);
+  const overlayContextRef = useRef<CanvasRenderingContext2D | null>(null);
   const [canvasInitialized, setCanvasInitialized] = useState<boolean>(false);
 
   // Initialize canvas
   useEffect(() => {
     // Set canvas context ref
-    if (canvasRef.current) {
+    if (canvasRef.current && overlayRef.current) {
       contextRef.current = canvasRef.current.getContext("2d");
+      overlayContextRef.current = overlayRef.current.getContext("2d");
       setCanvasInitialized(true);
       console.log("Canvas intialized.");
     }
   }, []);
 
-  return { canvasInitialized, canvasRef, contextRef };
+  return {
+    canvasInitialized,
+    canvasRef,
+    contextRef,
+    overlayRef,
+    overlayContextRef,
+  };
 };
 
 export default useInitCanvas;
