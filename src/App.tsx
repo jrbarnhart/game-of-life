4696225a744b4dd86bb58cc250c24d730ef1ea1c
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-04-04 17:25:16 -0400

    Style header and arrange elements in grid

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,10 @@ import GameCanvas from "./components/GameCanvas/GameCanvas";
 function App() {
   return (
     <>
-      <main className="text-neutral-50 h-full w-full">
-        <h1>Game of Life</h1>
-        <Controls />
+      <main className="text-neutral-50 h-full w-full grid grid-rows-[repeat(3,_min-content)] justify-items-center gap-y-5">
+        <h1 className="font-bold text-4xl pt-4">Game of Life</h1>
         <GameCanvas />
+        <Controls />
       </main>
     </>
   );
