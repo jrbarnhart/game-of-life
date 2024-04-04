cc03468fd0c7b19a9d0713b0d6c696b586112678
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-04-04 16:27:50 -0400

    Move svg code directly to component and style

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ import GameCanvas from "./components/GameCanvas/GameCanvas";
 function App() {
   return (
     <>
-      <h1>Game of Life</h1>
       <main className="text-neutral-50 h-full w-full">
+        <h1>Game of Life</h1>
         <Controls />
         <GameCanvas />
       </main>
--- src/components/Controls/Controls.tsx
+++ src/components/Controls/Controls.tsx
@@ -1,15 +1,62 @@
-import playIcon from "../../assets/play.svg";
-import pauseIcon from "../../assets/pause.svg";
-import nextIcon from "../../assets/next.svg";
-import stopIcon from "../../assets/stop.svg";
-
 const Controls = () => {
   return (
-    <div>
-      <button>{playIcon}</button>
-      <button>{pauseIcon}</button>
-      <button>{nextIcon}</button>
-      <button>{stopIcon}</button>
+    <div className="w-96 grid grid-flow-col">
+      <button className="h-10 text-neutral-950 hover:text-neutral-50 bg-neutral-700 active:bg-neutral-600 rounded-md border-2 border-black hover:border-orange-400 active:border-orange-500 grid items-center justify-items-center">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="24"
+          viewBox="0 -960 960 960"
+          width="24"
+          className="h-full w-full"
+        >
+          <path
+            fill="currentColor"
+            d="M320-200v-560l440 280-440 280Zm80-280Zm0 134 210-134-210-134v268Z"
+          />
+        </svg>
+      </button>
+      <button className="h-10 text-neutral-950 hover:text-neutral-50 bg-neutral-700 active:bg-neutral-600 rounded-md border-2 border-black hover:border-orange-400 active:border-orange-500 grid items-center justify-items-center">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="24"
+          viewBox="0 -960 960 960"
+          width="24"
+          className="h-full w-full"
+        >
+          <path
+            fill="currentColor"
+            d="M520-200v-560h240v560H520Zm-320 0v-560h240v560H200Zm400-80h80v-400h-80v400Zm-320 0h80v-400h-80v400Zm0-400v400-400Zm320 0v400-400Z"
+          />
+        </svg>
+      </button>
+      <button className="h-10 text-neutral-950 hover:text-neutral-50 bg-neutral-700 active:bg-neutral-600 rounded-md border-2 border-black hover:border-orange-400 active:border-orange-500 grid items-center justify-items-center">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="24"
+          viewBox="0 -960 960 960"
+          width="24"
+          className="h-full w-full"
+        >
+          <path
+            fill="currentColor"
+            d="M660-240v-480h80v480h-80Zm-440 0v-480l360 240-360 240Zm80-240Zm0 90 136-90-136-90v180Z"
+          />
+        </svg>
+      </button>
+      <button className="h-10 text-neutral-950 hover:text-neutral-50 bg-neutral-700 active:bg-neutral-600 rounded-md border-2 border-black hover:border-orange-400 active:border-orange-500 grid items-center justify-items-center">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="24"
+          viewBox="0 -960 960 960"
+          width="24"
+          className="h-full w-full"
+        >
+          <path
+            fill="currentColor"
+            d="M320-640v320-320Zm-80 400v-480h480v480H240Zm80-80h320v-320H320v320Z"
+          />
+        </svg>
+      </button>
     </div>
   );
 };
