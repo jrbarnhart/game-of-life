5687a087177f7f8c1f2ee988a5b6e68475eb1304
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-04-04 17:01:14 -0400

    Add drawing buttons and style them

--- src/components/Controls/Controls.tsx
+++ src/components/Controls/Controls.tsx
@@ -1,79 +1,95 @@
 const Controls = () => {
   return (
-    <div className="w-96 grid grid-flow-col">
-      <button
-        aria-label="play"
-        className="h-10 text-neutral-950 hover:text-neutral-50 bg-neutral-700 active:bg-neutral-600 rounded-md border-2 border-black hover:border-orange-400 active:border-orange-500 grid items-center justify-items-center"
-      >
-        <svg
-          aria-hidden
-          xmlns="http://www.w3.org/2000/svg"
-          height="24"
-          viewBox="0 -960 960 960"
-          width="24"
-          className="h-full w-full"
+    <>
+      <div className="w-96 p-1 grid grid-flow-col gap-x-1 bg-neutral-400 rounded-sm border-2 border-b-0 border-black">
+        <button
+          aria-label="play"
+          className="h-10 text-neutral-950 hover:text-neutral-50 bg-neutral-700 active:bg-neutral-600 rounded-md border-2 border-black hover:border-orange-400 active:border-orange-500 grid items-center justify-items-center"
         >
-          <path
-            fill="currentColor"
-            d="M320-200v-560l440 280-440 280Zm80-280Zm0 134 210-134-210-134v268Z"
-          />
-        </svg>
-      </button>
-      <button
-        aria-label="pause"
-        className="h-10 text-neutral-950 hover:text-neutral-50 bg-neutral-700 active:bg-neutral-600 rounded-md border-2 border-black hover:border-orange-400 active:border-orange-500 grid items-center justify-items-center"
-      >
-        <svg
-          aria-hidden
-          xmlns="http://www.w3.org/2000/svg"
-          height="24"
-          viewBox="0 -960 960 960"
-          width="24"
-          className="h-full w-full"
+          <svg
+            aria-hidden
+            xmlns="http://www.w3.org/2000/svg"
+            height="24"
+            viewBox="0 -960 960 960"
+            width="24"
+            className="h-full w-full"
+          >
+            <path
+              fill="currentColor"
+              d="M320-200v-560l440 280-440 280Zm80-280Zm0 134 210-134-210-134v268Z"
+            />
+          </svg>
+        </button>
+        <button
+          aria-label="pause"
+          className="h-10 text-neutral-950 hover:text-neutral-50 bg-neutral-700 active:bg-neutral-600 rounded-md border-2 border-black hover:border-orange-400 active:border-orange-500 grid items-center justify-items-center"
         >
-          <path
-            fill="currentColor"
-            d="M520-200v-560h240v560H520Zm-320 0v-560h240v560H200Zm400-80h80v-400h-80v400Zm-320 0h80v-400h-80v400Zm0-400v400-400Zm320 0v400-400Z"
-          />
-        </svg>
-      </button>
-      <button
-        aria-label="next frame"
-        className="h-10 text-neutral-950 hover:text-neutral-50 bg-neutral-700 active:bg-neutral-600 rounded-md border-2 border-black hover:border-orange-400 active:border-orange-500 grid items-center justify-items-center"
-      >
-        <svg
-          aria-hidden
-          xmlns="http://www.w3.org/2000/svg"
-          height="24"
-          viewBox="0 -960 960 960"
-          width="24"
-          className="h-full w-full"
+          <svg
+            aria-hidden
+            xmlns="http://www.w3.org/2000/svg"
+            height="24"
+            viewBox="0 -960 960 960"
+            width="24"
+            className="h-full w-full"
+          >
+            <path
+              fill="currentColor"
+              d="M520-200v-560h240v560H520Zm-320 0v-560h240v560H200Zm400-80h80v-400h-80v400Zm-320 0h80v-400h-80v400Zm0-400v400-400Zm320 0v400-400Z"
+            />
+          </svg>
+        </button>
+        <button
+          aria-label="next frame"
+          className="h-10 text-neutral-950 hover:text-neutral-50 bg-neutral-700 active:bg-neutral-600 rounded-md border-2 border-black hover:border-orange-400 active:border-orange-500 grid items-center justify-items-center"
         >
-          <path
-            fill="currentColor"
-            d="M660-240v-480h80v480h-80Zm-440 0v-480l360 240-360 240Zm80-240Zm0 90 136-90-136-90v180Z"
-          />
-        </svg>
-      </button>
-      <button
-        aria-label="stop"
-        className="h-10 text-neutral-950 hover:text-neutral-50 bg-neutral-700 active:bg-neutral-600 rounded-md border-2 border-black hover:border-orange-400 active:border-orange-500 grid items-center justify-items-center"
-      >
-        <svg
-          aria-hidden
-          xmlns="http://www.w3.org/2000/svg"
-          height="24"
-          viewBox="0 -960 960 960"
-          width="24"
-          className="h-full w-full"
+          <svg
+            aria-hidden
+            xmlns="http://www.w3.org/2000/svg"
+            height="24"
+            viewBox="0 -960 960 960"
+            width="24"
+            className="h-full w-full"
+          >
+            <path
+              fill="currentColor"
+              d="M660-240v-480h80v480h-80Zm-440 0v-480l360 240-360 240Zm80-240Zm0 90 136-90-136-90v180Z"
+            />
+          </svg>
+        </button>
+        <button
+          aria-label="stop"
+          className="h-10 text-neutral-950 hover:text-neutral-50 bg-neutral-700 active:bg-neutral-600 rounded-md border-2 border-black hover:border-orange-400 active:border-orange-500 grid items-center justify-items-center"
         >
-          <path
-            fill="currentColor"
-            d="M320-640v320-320Zm-80 400v-480h480v480H240Zm80-80h320v-320H320v320Z"
-          />
-        </svg>
-      </button>
-    </div>
+          <svg
+            aria-hidden
+            xmlns="http://www.w3.org/2000/svg"
+            height="24"
+            viewBox="0 -960 960 960"
+            width="24"
+            className="h-full w-full"
+          >
+            <path
+              fill="currentColor"
+              d="M320-640v320-320Zm-80 400v-480h480v480H240Zm80-80h320v-320H320v320Z"
+            />
+          </svg>
+        </button>
+      </div>
+      <div className="w-96 p-1 grid grid-flow-col gap-x-1 grid-cols-4 bg-neutral-400 border-2 border-t-0 border-black">
+        <button className="h-10 text-white hover:text-orange-400 bg-neutral-700 active:bg-neutral-600 rounded-md border-2 border-black hover:border-orange-400 active:border-orange-500 grid items-center justify-items-center">
+          Draw
+        </button>
+        <p className="h-10 text-lg font-bold text-black text-center grid items-center">
+          Mirror:{" "}
+        </p>
+        <button className="h-10 text-white hover:text-orange-400 bg-neutral-700 active:bg-neutral-600 rounded-md border-2 border-black hover:border-orange-400 active:border-orange-500 grid items-center justify-items-center">
+          Horizontal
+        </button>
+        <button className="h-10 text-white hover:text-orange-400 bg-neutral-700 active:bg-neutral-600 rounded-md border-2 border-black hover:border-orange-400 active:border-orange-500 grid items-center justify-items-center">
+          Vertical
+        </button>
+      </div>
+    </>
   );
 };
 
