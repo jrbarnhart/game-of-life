a5af6f42a6be561965d0835821807bfe1ff7c266
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-04-05 00:58:17 -0400

    Set gridWidth and Height props for GameCanvas

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
     <>
       <main className="text-neutral-50 h-full w-full grid grid-rows-[repeat(3,_min-content)] justify-items-center gap-y-5">
         <h1 className="font-bold text-4xl pt-4">Game of Life</h1>
-        <GameCanvas />
+        <GameCanvas gridWidth={20} gridHeight={20} />
         <Controls />
       </main>
     </>
