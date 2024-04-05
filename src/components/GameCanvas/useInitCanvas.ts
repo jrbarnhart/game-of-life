554cbff7e3b5cf8d78672edd50e04ee4ba132056
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-04-05 00:41:31 -0400

    Set canvas and container sizes on init

--- src/components/GameCanvas/useInitCanvas.ts
+++ src/components/GameCanvas/useInitCanvas.ts
@@ -1,22 +1,29 @@
 import { useEffect, useRef, useState } from "react";
 
-const useInitCanvas = () => {
+const useInitCanvas = (width: number, height: number) => {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
   const contextRef = useRef<CanvasRenderingContext2D | null>(null);
   const overlayRef = useRef<HTMLCanvasElement | null>(null);
   const overlayContextRef = useRef<CanvasRenderingContext2D | null>(null);
+  const containerRef = useRef<HTMLDivElement | null>(null);
   const [canvasInitialized, setCanvasInitialized] = useState<boolean>(false);
 
   // Initialize canvas
   useEffect(() => {
     // Set canvas context ref
-    if (canvasRef.current && overlayRef.current) {
+    if (canvasRef.current && overlayRef.current && containerRef.current) {
       contextRef.current = canvasRef.current.getContext("2d");
       overlayContextRef.current = overlayRef.current.getContext("2d");
+      canvasRef.current.width = width;
+      canvasRef.current.height = height;
+      overlayRef.current.width = width;
+      overlayRef.current.height = height;
+      containerRef.current.style.width = `${width.toString()}px`;
+      containerRef.current.style.height = `${height.toString()}px`;
       setCanvasInitialized(true);
       console.log("Canvas intialized.");
     }
-  }, []);
+  }, [height, width]);
 
   return {
     canvasInitialized,
@@ -24,6 +31,7 @@ const useInitCanvas = () => {
     contextRef,
     overlayRef,
     overlayContextRef,
+    containerRef,
   };
 };
 
