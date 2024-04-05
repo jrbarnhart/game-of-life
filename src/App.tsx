ba473253510d258176fa7f781599a103e525c523
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-04-05 04:15:37 -0400

    Remove controls

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
-import Controls from "./components/Controls/Controls";
 import GameOfLife from "./components/GameOfLife/GameOfLife";
 
 function App() {
   return (
     <>
-      <main className="text-neutral-50 h-full w-full grid grid-rows-[repeat(3,_min-content)] justify-items-center gap-y-5">
+      <main className="text-neutral-50 h-full w-full grid grid-rows-[repeat(2,_min-content)] justify-items-center gap-y-5">
         <h1 className="font-bold text-4xl pt-4">Game of Life</h1>
         <GameOfLife gridWidth={30} gridHeight={20} />
-        <Controls />
       </main>
     </>
   );
