457a6c566a5839e63e7790f613a65a34c3f8b401
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-04-06 17:57:00 -0400

    Add method for responding to overlay canvas clicks

--- src/components/GameOfLife/GameCanvas/GameCanvas.tsx
+++ src/components/GameOfLife/GameCanvas/GameCanvas.tsx
@@ -4,14 +4,25 @@ const GameCanvas = ({
   canvasRef,
   overlayRef,
   containerRef,
+  initialData,
 }: {
   canvasRef: React.MutableRefObject<HTMLCanvasElement | null>;
   overlayRef: React.MutableRefObject<HTMLCanvasElement | null>;
   containerRef: React.MutableRefObject<HTMLDivElement | null>;
+  initialData: Set<number>;
 }) => {
+  const addInitialData = () => {
+    // On click add the cell at the mouse location's index to initial data
+    console.log(initialData);
+  };
+
   return (
     <div ref={containerRef} className="relative">
-      <canvas className="absolute top-0 bg-transparent z-10" ref={overlayRef} />
+      <canvas
+        onClick={addInitialData}
+        className="absolute top-0 bg-transparent z-10"
+        ref={overlayRef}
+      />
       <canvas className="bg-black absolute top-0" ref={canvasRef} />
     </div>
   );
