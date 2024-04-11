f5f8de2b1dc64469f8d8c99bf1870a21338a2cf4
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-04-11 16:54:39 -0400

    Remove old props

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
     <>
       <main className="text-neutral-50 h-full w-full grid grid-rows-[repeat(2,_min-content)] justify-items-center gap-y-5">
         <h1 className="font-bold text-4xl pt-4">Game of Life</h1>
-        <GameOfLife gridWidth={45} gridHeight={30} />
+        <GameOfLife />
       </main>
     </>
   );
