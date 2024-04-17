68f93f02220b0ad6cbb67eae620980217309a703
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-04-17 16:56:56 -0400

    Style header and info button

--- src/components/InfoHeader/InfoHeader.tsx
+++ src/components/InfoHeader/InfoHeader.tsx
@@ -1,8 +1,12 @@
 const InfoHeader = () => {
   return (
-    <div className="grid grid-flow-col grid-cols-3">
-      <h1 className="col-start-2">Game of Life</h1>
-      <button className="col-start-3">?</button>
+    <div className="w-full grid grid-flow-col grid-cols-[1fr_min-content_1fr] items-center">
+      <h1 className="col-start-2 pt-4 pb-2 text-2xl text-nowrap text-center font-bold drop-shadow-lg shadow-black">
+        Conway&apos;s Game of Life
+      </h1>
+      <button className="col-start-3 mt-2 ml-2 justify-self-start font-bold text-sm bg-neutral-700 w-6 aspect-square rounded-full border-2 border-black">
+        <p className="">?</p>
+      </button>
     </div>
   );
 };
