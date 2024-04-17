e97f3d04646fcfdc147bc8afe5cdbd5a335eb527
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-04-17 17:03:46 -0400

    Style for hover focus and active and make header reactive

--- src/components/InfoHeader/InfoHeader.tsx
+++ src/components/InfoHeader/InfoHeader.tsx
@@ -1,10 +1,10 @@
 const InfoHeader = () => {
   return (
     <div className="w-full grid grid-flow-col grid-cols-[1fr_min-content_1fr] items-center">
-      <h1 className="col-start-2 pt-4 pb-2 text-2xl text-nowrap text-center font-bold drop-shadow-lg shadow-black">
+      <h1 className="col-start-2 pt-4 lg:pt-6 pb-2 text-2xl lg:text-4xl text-nowrap text-center font-bold drop-shadow-lg shadow-black">
         Conway&apos;s Game of Life
       </h1>
-      <button className="col-start-3 mt-2 ml-2 justify-self-start font-bold text-sm bg-neutral-700 w-6 aspect-square rounded-full border-2 border-black">
+      <button className="col-start-3 mt-2 lg:mt-3 ml-2 lg:ml-6 justify-self-start font-bold text-sm lg:text-lg bg-neutral-700 w-6 lg:w-8 aspect-square rounded-full border-2 border-black focus:outline-none focus:ring-2 focus:ring-orange-500 hover:text-orange-400 hover:border-orange-400 active:bg-neutral-600 active:border-orange-500">
         <p className="">?</p>
       </button>
     </div>
