df0332b24802ebffe2cc3cc90e482b0e5270ae2c
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-04-17 15:58:25 -0400

    Add simple header component with button

---
+++ src/components/InfoHeader/InfoHeader.tsx
@@ -0,0 +1,10 @@
+const InfoHeader = () => {
+  return (
+    <div className="grid grid-flow-col grid-cols-3">
+      <h1 className="col-start-2">Game of Life</h1>
+      <button className="col-start-3">?</button>
+    </div>
+  );
+};
+
+export default InfoHeader;
