2fc0a87aa48524109f066d1c15c20cf444dd5a78
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-04-17 17:21:29 -0400

    Add link prop to info header

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <main className="text-neutral-50 h-full w-full grid grid-rows-[repeat(2,_min-content)] justify-items-center gap-y-5">
-        <InfoHeader />
+        <InfoHeader link="https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life" />
         <GameOfLife />
       </main>
     </>
