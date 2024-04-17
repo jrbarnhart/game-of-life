d253e9accf443ecb8d69b812f7235a569784a6f5
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-04-17 16:01:03 -0400

    Replace header element with component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import GameOfLife from "./components/GameOfLife/GameOfLife";
+import InfoHeader from "./components/InfoHeader/InfoHeader";
 
 function App() {
   return (
     <>
       <main className="text-neutral-50 h-full w-full grid grid-rows-[repeat(2,_min-content)] justify-items-center gap-y-5">
-        <h1 className="font-bold text-4xl pt-4">Game of Life</h1>
+        <InfoHeader />
         <GameOfLife />
       </main>
     </>
