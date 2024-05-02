2e45f863fe05fca59aa86e5f84eb278b12565ce6
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-05-02 18:29:19 -0400

    Remove top padding and margin

--- src/components/InfoHeader/InfoHeader.tsx
+++ src/components/InfoHeader/InfoHeader.tsx
@@ -1,14 +1,14 @@
-const InfoHeader = ({ link }: { link: string }) => {
+const InfoHeader = ({ text, link }: { text: string; link: string }) => {
   return (
     <div className="w-full grid grid-flow-col items-center">
-      <h1 className="pt-4 lg:pt-6 pb-2 text-2xl lg:text-4xl text-nowrap text-center font-bold">
-        Conway&apos;s Game of Life
+      <h1 className=" text-2xl lg:text-4xl text-nowrap text-center font-bold">
+        {text}
       </h1>
       <a
         href={link}
         target="_blank"
         rel="noreferrer noopener"
-        className="mt-2 lg:mt-3 ml-2 lg:ml-6 justify-self-start font-bold text-center text-sm lg:text-lg bg-neutral-700 w-6 lg:w-8 aspect-square rounded-full border-2 border-black focus:outline-none focus:ring-2 focus:ring-orange-500 hover:text-orange-400 hover:border-orange-400 active:bg-neutral-600 active:border-orange-500"
+        className="ml-2 lg:ml-6 justify-self-start font-bold text-center text-sm lg:text-lg bg-neutral-700 w-6 lg:w-8 aspect-square rounded-full border-2 border-black focus:outline-none focus:ring-2 focus:ring-orange-500 hover:text-orange-400 hover:border-orange-400 active:bg-neutral-600 active:border-orange-500"
       >
         ?
       </a>
