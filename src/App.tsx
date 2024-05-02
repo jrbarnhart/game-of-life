4499bd0866e6e1964b118ec0cd81ee704c82e052
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-05-02 19:30:02 -0400

    Adjust grid layout

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import GameOfLife from "./components/GameOfLife/GameOfLife";
 function App() {
   return (
     <>
-      <main className="text-neutral-50 h-full w-full grid justify-items-center items-center">
+      <main className="text-neutral-50 h-full w-full grid justify-items-center">
         <GameOfLife />
       </main>
     </>
