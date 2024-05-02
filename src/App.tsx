dac8a3570865f3bffed9f2bb4c812bba74be7acf
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-05-02 18:39:08 -0400

    Remove header

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
 import GameOfLife from "./components/GameOfLife/GameOfLife";
-import InfoHeader from "./components/InfoHeader/InfoHeader";
 
 function App() {
   return (
     <>
       <main className="text-neutral-50 h-full w-full grid justify-items-center items-center">
-        <div className="absolute top-0 left-0 z-10 w-full">
-          <InfoHeader link="https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life" />
-        </div>
         <GameOfLife />
       </main>
     </>
