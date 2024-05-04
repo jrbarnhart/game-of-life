2b6d3fc3ab7698856e54c67f9a826800c55fb431
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-05-04 15:21:12 -0400

    Extract pseudoFS logic to custom hook

---
+++ src/components/GameOfLife/GameCanvas/usePseudoFS.ts
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+import useDisableFS from "./useDisableFS";
+
+const usePseudoFS = ({ debugNoFs = false }: { debugNoFs: boolean }) => {
+  const [outdatedBrowserFS, setOutdatedBrowserFS] = useState<boolean>(false);
+
+  useEffect(() => {
+    const handleOrientationChange = () => {
+      if (
+        screen.orientation.type === "portrait-primary" ||
+        screen.orientation.type === "portrait-secondary"
+      ) {
+        setOutdatedBrowserFS(false);
+      }
+    };
+
+    window.addEventListener("orientationchange", handleOrientationChange);
+
+    return () => {
+      window.removeEventListener("orientationchange", handleOrientationChange);
+    };
+  }, []);
+
+  // Used to disable fullscreen for testing purposes
+  useDisableFS({ use: debugNoFs });
+
+  return [outdatedBrowserFS, setOutdatedBrowserFS];
+};
+
+export default usePseudoFS;
