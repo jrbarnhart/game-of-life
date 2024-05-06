0c5e5bfbe2e416cf0b71761711587fd3f697c964
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-05-05 23:15:05 -0400

    Add state related to controls

--- src/components/GameOfLife/GameCanvas/useControlState.ts
+++ src/components/GameOfLife/GameCanvas/useControlState.ts
@@ -1,8 +1,22 @@
 import { useRef, useState } from "react";
 
 export interface ControlState {
+  fullscreen: boolean;
+  setFullscreen: React.Dispatch<React.SetStateAction<boolean>>;
   showOnlyDraw: boolean;
   setShowOnlyDraw: React.Dispatch<React.SetStateAction<boolean>>;
+  highlightPlay: boolean;
+  setHighlightPlay: React.Dispatch<React.SetStateAction<boolean>>;
+  highlightPause: boolean;
+  setHighlightPause: React.Dispatch<React.SetStateAction<boolean>>;
+  highlightDraw: boolean;
+  setHighlightDraw: React.Dispatch<React.SetStateAction<boolean>>;
+  highlightErase: number;
+  setHighlightErase: React.Dispatch<React.SetStateAction<number>>;
+  highlightMirrorX: number;
+  setHighlightMirrorX: React.Dispatch<React.SetStateAction<number>>;
+  highlightMirrorY: number;
+  setHighlightMirrorY: React.Dispatch<React.SetStateAction<number>>;
   isPlaying: React.MutableRefObject<boolean>;
   isPaused: React.MutableRefObject<boolean>;
   isDrawing: React.MutableRefObject<boolean>;
@@ -14,7 +28,16 @@ export interface ControlState {
 }
 
 const useControlState = () => {
+  const [fullscreen, setFullscreen] = useState<boolean>(false);
   const [showOnlyDraw, setShowOnlyDraw] = useState<boolean>(false);
+  const [highlightPlay, setHighlightPlay] = useState<boolean>(false);
+  const [highlightPause, setHighlightPause] = useState<boolean>(false);
+  const [highlightDraw, setHighlightDraw] = useState<boolean>(false);
+  // Number state: -1 dim, 0 normal, 1 highlight
+  const [highlightErase, setHighlightErase] = useState<number>(-1);
+  const [highlightMirrorX, setHighlightMirrorX] = useState<number>(-1);
+  const [highlightMirrorY, setHighlightMirrorY] = useState<number>(-1);
+
   const isPlaying = useRef(false);
   const isPaused = useRef(false);
   const isDrawing = useRef(false);
@@ -25,8 +48,22 @@ const useControlState = () => {
   const totalCells = useRef(1350); // 45 x 30 default
 
   const controlState: ControlState = {
+    fullscreen,
+    setFullscreen,
     showOnlyDraw,
     setShowOnlyDraw,
+    highlightPlay,
+    setHighlightPlay,
+    highlightPause,
+    setHighlightPause,
+    highlightDraw,
+    setHighlightDraw,
+    highlightErase,
+    setHighlightErase,
+    highlightMirrorX,
+    setHighlightMirrorX,
+    highlightMirrorY,
+    setHighlightMirrorY,
     isPlaying,
     isPaused,
     isDrawing,
