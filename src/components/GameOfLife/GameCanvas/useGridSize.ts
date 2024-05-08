533cc68f7e18e06c3d0913d0e7b9f81f996707f8
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-05-08 00:34:21 -0400

    Add another grid size option

--- src/components/GameOfLife/GameCanvas/useGridSize.ts
+++ src/components/GameOfLife/GameCanvas/useGridSize.ts
@@ -3,8 +3,9 @@ import { useRef } from "react";
 export const GRID_SIZES = [
   { x: 16, y: 9 },
   { x: 32, y: 18 },
+  { x: 64, y: 36 },
   { x: 128, y: 72 },
-  { x: 192, y: 108 },
+  { x: 384, y: 216 },
 ];
 
 export interface GridSize {
